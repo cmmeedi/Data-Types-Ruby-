aa9458d90457dbbab04aafacf62f7740c8242ec8
cmmeedi <89672577+cmmeedi@users.noreply.github.com>
2022-02-02 21:00:29 -0800

    Add files via upload

---
+++ Data-Types.rb
@@ -0,0 +1,12 @@
+#String is Plain Text
+name = "Mike Wizowsky"
+occupation = "Scarer"
+#age stores a number which doesn't require the quotation marks
+age = 35
+#decimals are referred to as Floating Points
+scare_rate = 88.9
+#True or False are called Booleans
+is_scary = True
+
+#nil (like null) is no value
+has_hair = nil
